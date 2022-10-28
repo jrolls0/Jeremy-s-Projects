c61318287c25071cd49223f833ecba500f876bac
jrolls0 <jrolls@umich.edu>
2022-10-28 16:09:19 -0400

    Initial commit

---
+++ EXAM 1 prep/EXAM 1 prep/main.cpp
@@ -0,0 +1,49 @@
+//
+//  main.cpp
+//  EXAM 1 prep
+//
+//  Created by Jeremy A. Rolls on 10/25/22.
+//
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+using namespace std;
+
+
+
+
+
+
+int main () {
+    return 0;
+}
+//Useful code below
+
+/*
+void readFromStream(istream &is, int N) {
+    string str;
+    while(is >> str) {
+        cout << str;
+        cout<<",";
+        for(int i=1; i<N; i++){
+            is >> str;
+        }
+    }
+}
+
+int main(int argc, char **argv){
+    if(argc != 2 && argc != 3) {
+        return 1;
+    }
+    int N = atoi(argv[1]);
+    if(argc == 3) {
+        ifstream is(argv[2]);
+        readFromStream(is, N);
+    }
+    else {
+       readFromStream(cin, N);
+    }
+return 0;
+}
+*/
